bcbc3bfd7a2256d806d30d640c7fdf1ff8c6a1e8
hoshit <hoshi@gclue.jp>
2013-04-04 09:08:03 +0900

    初回コミット.

---
+++ src/com/example/clocksample/ClockSample.java
@@ -0,0 +1,19 @@
+package com.example.clocksample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class ClockSample extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//Titleバーを消す
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		setContentView(R.layout.main);
+	}
+
+
+}
